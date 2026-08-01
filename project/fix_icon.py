1412026f0d334610bdac1e04abbb7d11fec6f89d
seyomnn <seymenalemin29@gmail.com>
2026-08-02 00:25:04 +0300

    Delete project directory

--- project/fix_icon.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-img = Image.open("ikon.ico").convert("RGBA")
-
-img.save("ikon.ico", format="ICO", sizes=[
-    (16, 16), (32, 32), (48, 48), (64, 64), (128, 128), (256, 256)
-])
-
-print("ikon.ico oluşturuldu!")
